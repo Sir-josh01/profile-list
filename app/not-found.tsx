ffce538b5cc79ebec06071b3bd0df5be0db026a8
Sir-josh <docsmile01@gmail.com>
2025-11-12 16:33:11 +0100

    Added image to the logo placeholder with link leading to the homepage

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,36 @@
+"use client"
 
 import React from "react";
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 
-export default function notFound() {
+export default function NotFound() {
+
+  const router  = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+    console.log("hello i ran")
+      router.push('/')
+    }, 3000); 
+  }, [router])
+
   return (
+    <>
     <div className="not-found">
       <h1>Opppps!!</h1> 
        <h2>that page cannot be found, this is a 404 Error</h2>
       <p>Go back to the <Link href="/" className="link">home-page</Link>
       </p>
     </div>
+    
+    <div className="not-found text-center mt-20">
+      <h1 className="text-4xl font-bold text-red-600">Oppps!!</h1> 
+      <h2 className="text-xl my-4">That page cannot be found. This is a 404 Error.</h2>
+      <p>Redirecting to the homepage in 3 seconds...</p>
+      <p>Or go back now: <Link href="/" className="link text-blue-500 hover:underline">Home Page</Link></p>
+    </div>
+    </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,20 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
+    <>
     <nav>
-      <div className='logo'>
-        <h1>Ninja_logo</h1>
+      <div className="logo">
+        <Link href="/">
+          <Image src="/globe.svg" alt="logo" width={40} height={40} />
+        </Link>
       </div>
+
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/ninjas">Ninja-Listing</Link>
     </nav>
-  )
+    </>
+  );
 }
