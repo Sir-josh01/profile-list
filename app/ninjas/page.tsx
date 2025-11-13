39a3915f696aa2f046d2964cc4b6773213695843
Sir-josh <docsmile01@gmail.com>
2025-11-13 15:16:53 +0100

    Added styling to the ninja page and created a details page

--- app/ninjas/page.tsx
+++ app/ninjas/page.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React from "react";
+import ninjas from "../data/data.json";
+import Link from 'next/link'
+import styles from '@/app/styles/ninja.module.css'
 
 export const metadata = {
   title: "Ninja Listing",
 };
 
 export default async function page() {
-  const response = await fetch("./data.json")
-  const data = await response.json()
+  const data = ninjas;
 
-  console.log(data)
-
-  
+  // console.log(data)
   return (
-  <>
-    <div>
-      
-    </div>
-  </>
-  )
+    <>
+      <div>
+        <h2 className="text-[20px] font-bold mb-2">Ninja List</h2>
+        <div>
+          {data.map((user) => (
+            <div key={user.id} className={styles.nameBox}>
+              <Link href='/ninjas/details'>
+                <h3  className={styles.nameLink}>{user.name}</h3>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
