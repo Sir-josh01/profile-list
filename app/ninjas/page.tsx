d16074a40588abec958666c5ae38557e473bbe33
Sir-josh <docsmile01@gmail.com>
2025-11-13 05:47:34 +0100

    Added a data.json file to fetch data from

--- app/ninjas/page.tsx
+++ app/ninjas/page.tsx
@@ -4,8 +4,33 @@ export const metadata = {
   title: "Ninja Listing",
 };
 
-export default function page() {
+export default async function page() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const data = await response.json()
+
+  console.log(data)
+
+  // type dataProps = {
+  //  data:{
+  //    id: number,
+  //   title?: string,
+  //   name: string
+  //  }[];
+  // }
   return (
-    <div>Hello, this is the page in pages</div>
+  <>
+    <div>
+      {data.map((user: {
+   data:{
+     id: number,
+    name: string
+   }[];
+  }) => (
+        <div key={user.id}>
+          {user.name}
+        </div>
+      ))}
+    </div>
+  </>
   )
 }
